032c606f281e4f2004c6c06f5b3cc8dbef37ddfb
SC2003 <46285019+SC2003@users.noreply.github.com>
2022-11-17 12:45:33 +0530

    design markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-document.querySelector("#root").textContent = `Sign-Up Form`;
+const checkPassword = () => {
+    const passInputMain = document.querySelector("#password");
+    const passInputSecond = document.querySelector("#conform-password");
+    if (passInputMain !== passInputSecond) {
+        console.log("both passwords dont match");
+        // console.log(passInputMain.value);
+        // console.log(passInputSecond.value);
+    } else {
+        console.log("both passwords match");
+        // console.log(passInputMain.value);
+        // console.log(passInputSecond.value);
+    }
+}
+// checkPassword();
